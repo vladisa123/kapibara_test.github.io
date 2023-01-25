6889a837dfdbe6ce69ac2cf63498e829fa484e02
vladisa_s <vstanp@mail.ru>
2023-01-25 17:55:29 +0300

    iniy

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('sqlite:db');
+    $pdo = new PDO('sqlite:https://github.com/vladisa123/kapibara_test.github.io/blob/dd89da64c9ea72799074e434fa82b8bccdc4c54b/db');
 } catch (PDOException $e) {
     die($e->getMessage());
 }
