2c6d58e902c8c34da429dca58a93a759c5c15757
vladisa_s <vstanp@mail.ru>
2023-01-26 01:48:26 +0300

    congig

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@ include 'func.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>CRUD приложение на PHP</title>
+    <title>Адресная Книга</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
